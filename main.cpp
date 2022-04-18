882fdf007085b9e5fe2026fc76fe62f1b1ffb85f
design_mode <103923329+hudawn@users.noreply.github.com>
2022-04-18 17:19:43 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+#include "abservice.cpp"
+#include "visitor.cpp"
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	//OBS::test();
+	VIS::test();
+	return 0;
+}
+
